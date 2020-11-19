7db17f0d080a1c4e8db877dc599b674e129a980b
jpeace08 <giau75076@st.vimaru.edu.vn>
2020-11-19 13:01:24 +0700

    Tada

---
+++ coutdown-timer/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setInterval(countdown, 1000);
+  });
+  
+  const countdown = () => {
+    const daysEl = document.querySelector("#days");
+    const hoursEl = document.querySelector("#hours");
+    const minsEl = document.querySelector("#mins");
+    const secondsEl = document.querySelector("#seconds");
+  
+    const newYearDate = new Date("1 Jan 2021");
+    const currentDate = new Date();
+  
+    const totalSecond = Math.floor((newYearDate - currentDate) / 1000);
+    const days = totalSecond / 3600 / 24;
+    const hours = (totalSecond / 3600) % 24;
+    const mins = totalSecond / 60;
+    const seconds = totalSecond % 60;
+  
+    daysEl.innerText = days;
+    hoursEl.innerText = hours;
+    minsEl.innerText = mins;
+    secondsEl.innerText = seconds;
+  };
